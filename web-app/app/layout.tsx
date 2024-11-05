b0c3095024e0e2162219c75d5d6946dce1c31a3f
Robocoders <team@blackbox.ai>
2024-11-05 20:29:02 +0000

    Update

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AppProvider } from "./AppContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -51,7 +52,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
